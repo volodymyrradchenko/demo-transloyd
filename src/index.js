ba29e85f6f56d2289dffea07652e734ba6a76999
rreckonerr <rreckonerr@gmail.com>
2018-12-10 10:09:39 +0200

    Initial commit from Create React App

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components/macro';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: coral;
+  }
+`;
+
+ReactDOM.render(
+  <div>
+    <GlobalStyle />
+    <App />
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
